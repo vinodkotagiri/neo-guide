9975f13360038a098df6aa32b25f6edb448c017c
Vinod Kotagiri <vinodkotagiri@icloud.com>
2025-01-25 20:25:39 +0530

    fix error

--- src/app/components/video-editor/ImageSlider.tsx
+++ src/app/components/video-editor/ImageSlider.tsx
@@ -4,10 +4,10 @@ import ImageCard from './ImageCard'
 function ImageSlider() {
   return (
     <div className='p-2 border-[1px] border-slate-700 rounded-md gap-2 flex flex-col bg-slate-900 flex-1 overflow-y-scroll'>
-      <ImageCard link='' text=''/>
-      <ImageCard link='' text=''/>
-      <ImageCard link='' text=''/>
-      <ImageCard link='' text=''/>
+      <ImageCard />
+      <ImageCard />
+      <ImageCard />
+      <ImageCard />
     </div>
   )
 }
